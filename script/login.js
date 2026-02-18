1d5ad9074646f25200c7bb867f0761d14555bce3
asm-ayesha <ayesha219333@gmail.com>
2026-02-18 18:24:05 +0600

    implement login and navigate to homepage

--- script/login.js
+++ script/login.js
@@ -1,1 +1,36 @@
-console.log('jhgghjdzxcx')
+document.getElementById('btn-login').addEventListener('click', function () {
+    // get the mobile number input
+
+    const inputMobile = document.getElementById('input-mobile');
+    const mobileNumber = inputMobile.value;
+    console.log(mobileNumber)
+
+
+    // get the pin input
+    const inputPin = document.getElementById('input-pin');
+    const pinNumber = inputPin.value;
+    console.log(pinNumber)
+
+
+    // match pin & mobile number
+
+    if (mobileNumber == '01234567890' && pinNumber == '1234') {
+        // ture::> alert >  homepage
+        alert("login Success")
+
+        window.location.assign('/home.html')
+        // clean the text area
+        inputMobile.value = '';
+        inputPin.value = '';
+
+    }
+    else {
+
+        // false::> alert >  return
+
+        alert('login failed')
+        return
+    }
+
+
+})
